d8d44c3ea004b7aa1afc5953610f16974cfe2c60
ikramulhasan1 <hasanikramul926@gmail.com>
2023-09-02 15:58:40 +0600

    Product seeder and product factory created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
@@ -18,5 +17,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('products')->insert([
+            'title' => 'Test Product',
+            'description' => 'Test Product Description',
+            'image' => 'images/test.jpg',
+        ]);
+
+
     }
 }

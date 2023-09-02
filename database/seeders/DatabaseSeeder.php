133f323527a7cc97711ece33d4e1676b421190c4
ikramulhasan1 <hasanikramul926@gmail.com>
2023-09-02 16:20:39 +0600

    Product table new data seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,24 +5,15 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Database\Seeders\ProductSeeder;
 
 class DatabaseSeeder extends Seeder
 {
 
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        DB::table('products')->insert([
-            'title' => 'Test Product',
-            'description' => 'Test Product Description',
-            'image' => 'images/test.jpg',
+        $this->call([
+            ProductSeeder::class
         ]);
-
-
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,16 +2,19 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            'title' => 'Test Product',
+            'description' => 'Test Product Description',
+            'image' => 'images/test.jpg',
+        ]);
     }
 }
